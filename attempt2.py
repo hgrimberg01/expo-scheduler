6eac99532b336c7ce93f42690aab493ad4029100
Howard Grimberg <hgrimberg@ku.edu>
2014-05-06 12:32:59 -0500

    Added variable number of concurent things per time slot

--- attempt2.py
+++ attempt2.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 from random import randint,choice
 from copy import copy as ShallowCopy
+from copy import deepcopy as DeepCopy
+import math
 
-test_bindings = [('Team A', 'Event A'), ('Team B', 'Event A'), ('Team C', 'Event A'),
-            ('Team A', 'Event B'), ('Team B', 'Event B'), ('Team C', 'Event B'),
-            ('Team B', 'Event C'), ('Team C', 'Event C')]
+import csv
+with open("team_event_elem_school.csv",'r') as f:
+        r = csv.reader(f)
+        test_bindings = [(row[0].strip(), row[1].strip()) for row in r ]
 
-test_bindings = []
-for i in range(58):
-    p = 'Team '+str(i)
-    a = (p,'Event A')
-    test_bindings.append(a)
+#test_bindings = [('Team A', 'Event A'), ('Team B', 'Event A'), ('Team C', 'Event A'),
+#            ('Team A', 'Event B'), ('Team B', 'Event B'), ('Team C', 'Event B'),
+#            ('Team B', 'Event C'), ('Team C', 'Event C')]
+
+# Number of Time Slots (Hours * 60 ) / Minutes Per Time Slot
 TIME_SLOTS = (5 * 60 // 5)
-EVENTS = 3
-TEAMS = 3
+
+# Number of teams that can be scheduled to a slot
+NUMBER_PER_SLOT = 5
 
 class Slot:
     def __init__(self, time, team, event):
@@ -58,25 +62,17 @@ class Chromosome:
         not_all_teams_scheduled = False
 
 
+        for i,k in enumerate(self.times):
+            if not len(k) > NUMBER_PER_SLOT:
+                fitness += len(k)
 
-
-        for team_name, time_slot in self.schedule.items():
-            slts = self.times[time_slot]
-
-            for name in slts:
-                if(name != team_name):
+        for team_tuple, slot in self.schedule.items():
+            for team_tuple2, slot2 in self.schedule.items():
+                if team_tuple[0] == team_tuple2[0] and slot == slot2:
                     team_conflict = True
-                    break
                 else:
                     fitness += 1
-            #if(team_conflict):
-            #    break;
 
-       # if not team_conflict :
-       #     fitness += 1
-
-        #if not not_all_teams_scheduled:
-        #    fitness += 1
 
 
         return fitness
@@ -86,15 +82,13 @@ class Chromosome:
         if(self.parent == None):
             # No parent, so randomly build a schedule,
             for binding in self.bindings:
-                rand_pos = randint(0, TIME_SLOTS - 1)
+                rand_pos = randint(1, TIME_SLOTS) - 1
                 selection = binding
                 self.schedule[selection] = rand_pos
                 self.times[rand_pos].append(selection)
         else:
             pass
 
-    def mutate(self, size):
-        pass
 
     def rebuild_times_from_sched(self):
         self.times = [ []  for x in range(TIME_SLOTS)]
@@ -116,10 +110,12 @@ class Chromosome:
                         new_sched[team2] = time2
                         self.times[time2].append(team2)
             self.schedule = new_sched
+            self.rebuild_times_from_sched()
         elif(strategy == 'RAND'):
-            new_sched = ShallowCopy(self.parent.schedule)
+            new_sched = DeepCopy(self.parent.schedule)
 
             #Choose N(size) of from second parent to put into first parent
+
             for i in range(size):
                 rand_key_one = choice(list(new_sched.keys()))
                 rand_key_two = choice(list(self.parent_two.schedule.keys()))
@@ -127,11 +123,12 @@ class Chromosome:
                 val_one = new_sched[rand_key_one]
                 val_two = self.parent_two.schedule[rand_key_two]
 
-                self.schedule[rand_key_two] = val_one
-                self.schedule[rand_key_one] = val_two
+                new_sched[rand_key_two] = val_one
+                new_sched[rand_key_one] = val_two
 
             self.schedule = new_sched
             self.rebuild_times_from_sched()
+
 
 
 
@@ -144,28 +141,28 @@ class Chromosome:
             for i in range(size):
                 rand_key_one = choice(list(new_sched.keys()))
 
-                rd = randint(0,TIME_SLOTS-1)
+                rd = randint(1,TIME_SLOTS) - 1
                 old_val = self.schedule[rand_key_one]
 
                 self.times[old_val].remove(rand_key_one)
                 self.schedule[rand_key_one] = rd
                 self.times[rd].append(rand_key_one)
         elif(strategy == 'INTELLIGENT'):
-            open_slot = self.find_open_slot()
-            for r in self.times:
-                if(len(r) > 1):
-                    reloc = r[1:]
-                    r = r[:1]
+            for i,r in enumerate(self.times):
+                if(len(r) > NUMBER_PER_SLOT):
+                    reloc = r[NUMBER_PER_SLOT:]
+                    r = r[:NUMBER_PER_SLOT]
                     for mv in reloc:
-                        rnd =  open_slot
-                        self.schedule[mv] = rnd
+
+                        self.schedule[mv] = self.find_open_slot()
+
+                        self.rebuild_times_from_sched()
 
 
 
     def find_open_slot(self):
         for i,r in enumerate(self.times):
-            if len(r) == 0:
-                self.open_slots.append(i)
+            if len(r) < NUMBER_PER_SLOT:
                 return i
 
 
@@ -178,11 +175,6 @@ class Chromosome:
             print ("%s  %s" % team, slot)
         return
 
-    def __cmp__(self, other):
-        return cmp(-1*self.get_fitness(), -1*other.get_fitness())
-
-    def __lt__(self,other):
-        return self.get_fitness() < other.get_fitness()
 
 class ScheduleGenerator:
 
@@ -199,37 +191,58 @@ class ScheduleGenerator:
         init2 = Chromosome(self.bindings)
         if1 = init.get_fitness()
         if2 = init2.get_fitness()
-
+        gens = 1
         if(if1 > if2):
             best_fitness = if1
+            parents.insert(0, init)
+            parents.append(init2)
         elif (if2 >= if1):
             best_fitness = if2
+            parents.insert(0, init2)
+            parents.append(init)
 
-        parents.append(init)
-        while (best_fitness != len(self.bindings)):
-            rand_parent_one = parents[randint(0,len(parents)-1)]
-            rand_parent_two = parents[randint(0,len(parents)-1)]
+        ideal_fitness = (len(self.bindings) ** 2)
+        while (best_fitness < ideal_fitness):
+            rand_parent_one = parents[randint(1,len(parents))-1]
+            rand_parent_two = parents[randint(1,len(parents))-1]
             new_chromosome = Chromosome(init.bindings, rand_parent_one,rand_parent_two)
-            mt = (58 - best_fitness) // 2
+            mt = math.ceil((len(self.bindings) - best_fitness) / 2)
+            if(mt <= 0):
+                mt = 1
+            #print(str(mt))
             new_chromosome.crossover(mt)
             strategy = 'RAND'
-            if(best_fitness > len(self.bindings) - 2):
+            if(best_fitness >= round( ideal_fitness * 0.85) ):
                 strategy = 'INTELLIGENT'
-
+            gens += 1
             new_chromosome.mutate(mt,strategy)
             fitness = new_chromosome.get_fitness()
-            print('Best: '+str(best_fitness))
-#            print('--------')
-#            new_chromosome.print_sched()
+           # print('Best: '+str(best_fitness))
+           # print('--------')
+           # new_chromosome.print_sched()
             if(fitness >= best_fitness):
                 best_fitness = fitness
                 parents.insert(0,new_chromosome)
 
             #print(parents)
 
-        parents[0].print_sched()
+       # parents[0].print_sched()
+        sched = parents[0].schedule
+        from collections import OrderedDict
+        fsched = OrderedDict(sorted(sched.items(), key=lambda t: t[0]))
+        counts = {}
+        for k,v in fsched.items():
+            if v not in counts:
+                counts[v] = 1
+            else:
+                counts[v] += 1
+            a = '%s  %s' % (k,v)
+            print(a)
 
+        print (counts)
+        print(parents[0].get_fitness())
 
+        print('Generation :' + str(gens))
 
 
 
@@ -239,7 +252,6 @@ class ScheduleGenerator:
 
 
 gen = ScheduleGenerator(test_bindings)
-
 gen.gen_sched()
 
 
